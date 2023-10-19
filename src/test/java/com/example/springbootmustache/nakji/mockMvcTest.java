5775813a411e4528a6d7d4f625863c7cd9404ef0
Nakji <yuitfhk@gmail.com>
2023-10-19 17:41:17 +0900

    movcMvc 테스트 소스 추가

---
+++ src/test/java/com/example/springbootmustache/nakji/mockMvcTest.java
@@ -0,0 +1,39 @@
+package com.example.springbootmustache.nakji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(NakjiController.class)
+public class mockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private class PageView {
+        private String title;
+        private String content;
+
+        // Getter and Setter methods
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        searchExample ex = new searchExample();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/convert")
+                        .contentType("application/json")
+                        .content("{\"title\":\"Sample Page\",\"content\":\"Sample Content\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Sample Page"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value("Sample Content"));
+
+
+    }
+
+
+}
