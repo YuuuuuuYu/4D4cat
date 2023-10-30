8945d02c656d22744ddac98f0ad02ccc9f55ae43
Nakji <yeonghak77@kangwon.ac.kr>
2023-10-30 17:11:05 +0900

    검색결과 로직 개선

--- src/main/java/com/example/springbootmustache/nakji/NakjiController.java
+++ src/main/java/com/example/springbootmustache/nakji/NakjiController.java
@@ -1,17 +1,14 @@
 package com.example.springbootmustache.nakji;
 
-import com.example.springbootmustache.nakji.common.CommonService;
-import com.example.springbootmustache.nakji.config.NakjiProperty;
+import com.example.springbootmustache.nakji.api.service.ApiService;
 import com.example.springbootmustache.nakji.model.PageView;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.HttpURLConnection;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -19,53 +16,13 @@ import java.util.List;
 public class NakjiController {
 
     @Autowired
-    NakjiProperty nakji;
-
-    @Autowired
-    CommonService commonService;
+    ApiService apiService;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "Hello, World!");
         model.addAttribute("message", "This is a mustache template.");
         return "nakji/index";
-    }
-
-    @GetMapping("/api/naver")
-    public String naverSearch(Model model, @RequestParam(name = "query", required = false) String query) {
-        JsonNode resultJson = null;
-        JsonNode jsonMap = null;
-        HttpURLConnection connection = null;
-        List<PageView> returnPage = null;
-        int responseCode = -1;
-
-        try {
-            connection = nakji.naverSearchConnection("", query);
-            responseCode = connection.getResponseCode();
-
-            if (responseCode == HttpURLConnection.HTTP_OK){
-                resultJson = commonService.readBody(connection.getInputStream());
-                jsonMap = resultJson.findValue("items");
-                returnPage = new ArrayList<>();
-
-                Iterator<JsonNode> elements = jsonMap.elements();
-                JsonNode item = null;
-                while (elements.hasNext()) {
-                    item = elements.next();
-                    returnPage.add(new PageView(item.get("title").asText(), item.get("description").asText(), item.get("link").asText()));
-                }
-            }
-
-        } catch(Exception e) {
-          e.printStackTrace();
-
-        } finally {
-            if (connection != null) connection.disconnect();
-        }
-
-        model.addAttribute("pages", returnPage);
-
-        return "nakji/page/pageView";
     }
 
 }
---
+++ src/main/java/com/example/springbootmustache/nakji/api/controller/ApiController.java
@@ -0,0 +1,32 @@
+package com.example.springbootmustache.nakji.api.controller;
+
+import com.example.springbootmustache.nakji.api.service.ApiService;
+import com.example.springbootmustache.nakji.model.PageView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nakji/api")
+public class ApiController {
+
+    @Autowired
+    ApiService apiService;
+
+    @GetMapping("/")
+    public PageView index(PageView pageView) {
+
+        return pageView;
+    }
+
+    @GetMapping("/naver.text")
+    public String naverSearchApi(@RequestParam(name = "query", required = false) String query) {
+        List<PageView> pageResult = apiService.naverSearch(query);
+
+        return apiService.searchResultToTemplate(pageResult);
+    }
+}
