fd01ce88b8373d6acd5a6410682ae83b6a05719f
Nakji <yeonghak77@kangwon.ac.kr>
2023-10-31 16:45:20 +0900

    구글 검색 추가

--- src/main/java/com/example/springbootmustache/nakji/api/service/ApiService.java
+++ src/main/java/com/example/springbootmustache/nakji/api/service/ApiService.java
@@ -55,6 +55,40 @@ public class ApiService {
         return returnPage;
     }
 
+    public List<PageView> googleSearch(String query) {
+        JsonNode resultJson = null;
+        JsonNode jsonMap = null;
+        HttpURLConnection connection = null;
+        List<PageView> returnPage = null;
+        int responseCode = -1;
+
+        try {
+            connection = nakji.googleSearchConnection(query);
+            responseCode = connection.getResponseCode();
+            returnPage = new ArrayList<>();
+
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                resultJson = commonService.readBody(connection.getInputStream());
+                jsonMap = resultJson.findValue("items");
+
+                Iterator<JsonNode> elements = jsonMap.elements();
+                JsonNode item = null;
+                while (elements.hasNext()) {
+                    item = elements.next();
+                    returnPage.add(new PageView(item.get("title").asText(), item.get("snippet").asText(), item.get("link").asText()));
+                }
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            if (connection != null) connection.disconnect();
+        }
+
+        return returnPage;
+    }
+
     public String searchResultToTemplate(List<PageView> list) {
         StringBuilder result = new StringBuilder();
 
