17a87d48d4b150ff8679e7fd312fc5ae66c25a9a
Nakji <yuitfhk@gmail.com>
2023-11-01 15:49:11 +0900

    검색 로직 처리 개선 및 시각 효과 추가

--- src/main/resources/static/nakji/js/nakji.js
+++ src/main/resources/static/nakji/js/nakji.js
@@ -1,83 +1,82 @@
-var nakji = {
-
-    init: function () {
-        var _this = this;
-        document.getElementById('btn_search').addEventListener('click', function () {
-            //_this.searchNaver();
-        });
-        _this.setSearchEngine();
-    },
-    setSearchEngine: function() {
-        let engineFrame = document.getElementById('search-engine');
-        const searchBtns = [
-            { id: 'naver', img_on: '/assets/img/search/icon_naver.png', img_click: '/assets/img/search/icon_naver_click.png' },
-            { id: 'google', img_on: '/assets/img/search/icon_google.png', img_click: '/assets/img/search/icon_google_click.png' }
-        ];
-
-        for (const btn of searchBtns) {
-            const button = document.createElement('button');
-            button.type = 'button';
-            button.className = 'btn';
-            button.id = 'btn_search_'+btn.id;
-
-            let img = document.createElement('img');
-            img.src = btn.img_on;
-            img.alt = btn.id;
-
-            this.toggleSearchEngine(button, img, btn.img_on, btn.img_click);
-            button.appendChild(img);
-            engineFrame.appendChild(button);
-        }
-    },
-    toggleSearchEngine: function(btn, img, prevImg, activeImg) {
-        btn.addEventListener('click', function () {
-            let activeImgs = document.querySelectorAll('button.active');
-            activeImgs.forEach(target => target.classList.remove('active'));
-            btn.classList.add('active');
-        });
-        /* 검토 필요
-        btn.addEventListener('classListchange', function (e) {console.log(e);
-            const targetClass = e.detail;
-            if (targetClass !== 'active') return;
-
-            img.src = prevImg;
-            img.src = activeImg;
-
-        });
-         */
-    },
-    searchNaver: async function () {
-        let query = document.getElementById("ipt_search").value;
-        let resultDiv = document.getElementById("searchResult");
-        const response = await fetch("/nakji/api/naver.text?query="+query)
-
-            .then(response => {
-                if (response.ok) {
-                    return response.text();
-
-                } else {
-                    throw new Error("네트워크 응답이 정상적이지 않습니다");
-                }
-        });
-
-        resultDiv.innerHTML = response;
-    },
-    searchGoogle: async function () {
-        let query = document.getElementById("ipt_search").value;
-        let resultDiv = document.getElementById("searchResult");
-        const response = await fetch("/nakji/api/google?query="+query)
-
-            .then(response => {
-                if (response.ok) {
-                    return response.text();
-
-                } else {
-                    throw new Error("네트워크 응답이 정상적이지 않습니다");
-                }
-            });
-
-        resultDiv.innerHTML = response;
-    }
+window.onload = function() {
+    setSearchEngine();
+    toggleSearchEngine();
 };
+function setSearchEngine() {
+    let engineFrame = document.getElementById('search-engine');
+    const searchBtns = [
+        { id: 'naver', img_on: '/assets/img/search/icon_naver.png', func: 'searchNaver' },
+        { id: 'google', img_on: '/assets/img/search/icon_google.png', func: 'searchGoogle' }
+    ];
 
-nakji.init();
+    for (const btn of searchBtns) {
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.className = 'btn';
+        button.id = 'btn_search_'+btn.id;
+        button.name = btn.id;
+
+        let img = document.createElement('img');
+        img.src = btn.img_on;
+        img.alt = btn.id;
+
+        button.appendChild(img);
+        engineFrame.appendChild(button);
+    }
+}
+function toggleSearchEngine() {
+    const btns = document.querySelectorAll('div#search-engine button');
+    btns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            if (btn.classList.contains('active')) return;
+
+            const img = btn.querySelector('img');
+            let activeBtn = document.querySelectorAll('button.active');
+
+            btn.classList.add('active');
+            toggleSearchImg(img);
+
+            activeBtn.forEach(target => {
+                target.classList.remove('active');
+                toggleSearchImg(target.querySelector('img'));
+            });
+        });
+    });
+}
+function toggleSearchImg(target) {
+    const imgSrc = target.src;
+    if (!imgSrc.includes('pick')) {
+        const resultSrc = imgSrc.split('.');
+        target.src = resultSrc[0] + '_pick.' + resultSrc[1];
+    } else {
+        target.src = imgSrc.replace('_pick', '');
+    }
+}
+async function doSearch() {
+    const engine = document.querySelector('div#search-engine button.active');
+    let query = document.getElementById("ipt_search").value;
+    let resultDiv = document.getElementById("searchResult");
+
+    if(engine == null) {
+        alert('검색 엔진을 선택해주세요.');
+        return;
+    }
+
+    if(query === '') {
+        alert('검색어를 입력해주세요.');
+        return;
+    }
+
+    let url = '/nakji/api/'+engine.name+'?query='+query;
+    const response = await fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+
+            } else {
+                throw new Error("네트워크 응답이 정상적이지 않습니다");
+            }
+        });
+
+    resultDiv.innerHTML = response;
+}
