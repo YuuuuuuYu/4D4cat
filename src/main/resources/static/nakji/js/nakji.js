8c4e039838454bbee57fcde5c9e9429194533490
Nakji <yuitfhk@gmail.com>
2023-11-16 17:10:51 +0900

    공통 함수 추가

--- src/main/resources/static/nakji/js/nakji.js
+++ src/main/resources/static/nakji/js/nakji.js
@@ -2,3 +2,24 @@ window.onload = function() {
     setSearchEngine();
     toggleSearchEngine();
 };
+
+function renderingPage(form) {
+    let resultDiv = document.getElementById("page");
+    const htmlForm = form;
+
+    resultDiv.innerHTML = htmlForm;
+}
+
+function callAPI(url) {
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+
+            } else {
+                throw new Error("네트워크 응답이 정상적이지 않습니다");
+            }
+        });
+
+    return false;
+}
