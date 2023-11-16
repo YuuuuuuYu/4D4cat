30b5279849544f677561f01664b890d5d57d684f
Nakji <yeonghak77@kangwon.ac.kr>
2023-11-16 17:11:53 +0900

    게시판 관련 소스 추가, 미구현

---
+++ src/main/java/com/example/springbootmustache/nakji/board/controller/boardController.java
@@ -0,0 +1,15 @@
+package com.example.springbootmustache.nakji.board.controller;
+
+import com.example.springbootmustache.nakji.model.PageView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/nakji/board")
+public class boardController {
+
+    @GetMapping("/")
+    public PageView index(PageView pageView) {
+
+        return pageView;
+    }
+}
--- src/main/java/com/example/springbootmustache/nakji/model/PageView.java
+++ src/main/java/com/example/springbootmustache/nakji/model/PageView.java
@@ -1,28 +1,9 @@
 package com.example.springbootmustache.nakji.model;
 
 public class PageView {
+    private Long id;
     private String title;
     private String content;
-    private String link;
+    private Long viewCount;
 
-    public PageView(String title, String content, String link) {
-        this.title = title;
-        this.content = content;
-        this.link = link;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public String getContent() {
-        return content;
-    }
-    public String getLink() {
-        return link;
-    }
-
-    @Override
-    public String toString() {
-        return "title: " + title + ", content: " + content + ", link: " + link;
-    }
 }
