b1411742717970a68c7a8dd5826641af702cf90c
Nakji <yuitfhk@gmail.com>
2023-11-21 17:30:31 +0900

    구글 oAuth 발급 테스트

--- src/test/java/com/example/springbootmustache/nakji/commonProperties.java
+++ src/test/java/com/example/springbootmustache/nakji/commonProperties.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.json.JSONException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.List;
 
 @SuppressWarnings("unchecked")
@@ -20,8 +20,8 @@ public class commonProperties {
     //private final static String KEY = System.getProperty("api.naver.key");
     protected final static String NAVER_ID = "DXUdYJLVPWaC2kxD_PUm";
     protected final static String NAVER_KEY = "SFvAihes1e";
-    //private final static String GOOGLE_ID = "895857791329-mi3g48445im7h7o1lceoslcef0vgg30u.apps.googleusercontent.com/";
-    //private final static String GOOGLE_KEY = "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";
+    protected final static String GOOGLE_CLIENT_ID = "895857791329-mi3g48445im7h7o1lceoslcef0vgg30u.apps.googleusercontent.com";
+    protected final static String GOOGLE_CLIENT_SECRET = "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";
     protected final static String GOOGLE_KEY = "AIzaSyBxdEGZKno9zIewJzFjMzCqngx_dxuWdHE";
     protected final static String GOOGLE_CX = "c7afc5a33141f45f6";
 
@@ -41,8 +41,11 @@ public class commonProperties {
 
             return jsonNode;
         } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+            System.out.println(e);
+
         }
+
+        return null;
     }
 
     protected void printJson(String str) throws JsonProcessingException, JSONException {
@@ -56,5 +59,77 @@ public class commonProperties {
         List<String> list = objectMapper.readValue(str, List.class);
 
         System.out.println(objectMapper.writeValueAsString(list));
+    }
+
+    protected String getActiveCode() {
+        String host = "https://accounts.google.com/o/oauth2/v2/auth";
+        String scope = "https://www.googleapis.com/auth/bigquery.readonly";
+        String redirect_uri = "http://localhost:8888/nakji/code";
+        String response_type = "code";
+        StringBuilder text = new StringBuilder();
+        String activeCode = null;
+
+        URL url = null;
+        HttpURLConnection connection = null;
+        try {
+            text.append(host);
+            text.append("?scope=").append(scope).append("&response_type=").append(response_type)
+                    .append("&client_id=").append(GOOGLE_CLIENT_ID).append("&redirect_uri=").append(redirect_uri);
+            url = new URL(text.toString());
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode);
+
+            String redirectedUrl = connection.getURL().getQuery().split("&")[0];
+            System.out.println("Redirected URL: " + redirectedUrl);
+
+            if (redirectedUrl.contains("authError")) return activeCode;
+
+            activeCode = redirectedUrl.replace("code=", "");
+            System.out.println(activeCode);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            if(connection != null) connection.disconnect();
+        }
+
+        return activeCode;
+    }
+
+    protected String getAccessToken() {
+        String tokenUrl = "https://oauth2.googleapis.com/token";
+
+        URL url = null;
+        HttpURLConnection connection = null;
+        try {
+            String requestBody = "{\"Authorization\":\"Basic " + Base64.getEncoder().encodeToString((GOOGLE_CLIENT_ID + ":" + GOOGLE_CLIENT_SECRET).getBytes()) + "\"}";
+            url = new URL(tokenUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "x-www-form-urlencoded; charset=UTF-8");
+            connection.setRequestProperty("Authorization", requestBody);
+            connection.setRequestProperty("Content-Length", Integer.toString(requestBody.getBytes().length)); // Content-Length 헤더 추가
+
+            // 요청 본문 전송
+            connection.setDoOutput(true);
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                outputStream.write(requestBody.getBytes("UTF-8"));
+            }
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode);
+            System.out.println(readBody(connection.getInputStream()));
+
+        } catch(Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            if(connection != null) connection.disconnect();
+        }
+
+        return "t";
     }
 }
