a8b40341a5e73c1cce14bc9c7202c128b2f1fd86
Nakji <yuitfhk@gmail.com>
2023-11-27 17:12:42 +0900

    AccessToken으로 api 호출 테스트

--- src/test/java/com/example/springbootmustache/nakji/topTermsExample.java
+++ src/test/java/com/example/springbootmustache/nakji/topTermsExample.java
@@ -1,56 +1,48 @@
 package com.example.springbootmustache.nakji;
 
+import com.example.springbootmustache.nakji.Iface.GoogleTopTermsClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import feign.Feign;
+import feign.Response;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-
-import java.util.stream.Collectors;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class topTermsExample extends commonProperties {
 
     @Test
     void googleTopTermsTest() throws JsonProcessingException, JSONException {
-        String projectId = "crested-column-401906";
-        String datasetId = "_4b4e36620224ba95c01416e7d9d98a9ff1e48d38";
-        String jobId = "bquxjob_79cc349e_18bc75a0643";
-        String host = "https://bigquery.googleapis.com/bigquery/v2/projects/"+projectId+"/datasets/"+datasetId;
+        String projectId = "mapping-1559551857011";
+        String datasetId = "_9996b01ee4f09b65b48ee6d884dc7087a7462f34";
+        String jobId = "bquxjob_33a9efe9_18c0fc537ce";
+        String url = "https://bigquery.googleapis.com";
         String query = "SELECT refresh_date AS Day, term AS Top_Term, rank FROM `bigquery-public-data.google_trends.top_terms` WHERE rank <= 5 AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY) GROUP BY Day, Top_Term, rank ORDER BY rank";
-        StringBuilder text = new StringBuilder();
+        String token = "ya29.a0AfB_byCneYYMjgpCq1egKXPB56eHj5bUZfrkPgAEtsgnsB9cniWViHN9GSJxG3SC0_4FJdBvKEhUGq3l0Yd4MIUjNS_ssdh2K3jpHuqNzyREdlChPX7bjjuY7Wz4VjV5SnoelX-KxdC-i_k383opq3-QvnxzZvgUdnyIaCgYKAaUSAQ4SFQHGX2Mi2Hw6gttZNnKpiRm3osvP8g0171";
         JsonNode resultJson = null;
-        JsonNode jsonMap = null;
+        JsonNode schema = null;
+        JsonNode rows = null;
 
-        // post로 테스트 다시 해봐야함
-        URL url = null;
-        HttpURLConnection connection = null;
-        try {
-            text.append(host);
-            text.append("?key=").append(GOOGLE_KEY).append("&cx=").append(GOOGLE_CX);
-            url = new URL(text.toString());
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        GoogleTopTermsClient client = Feign.builder().target(GoogleTopTermsClient.class, url);
 
-            int responseCode = connection.getResponseCode();
-            System.out.println(responseCode);
+        try (Response response = client.getQuery(projectId, jobId, token)) {
+            System.out.println(response.request());
+            if (response.status() == 200) {
+                resultJson = readBody(response.body().asInputStream());
+                System.out.println(resultJson);
 
-            resultJson = readBody(connection.getInputStream());
-            System.out.println(resultJson);
+            } else {
+                System.err.println("Error Response: " + response.toString());
+            }
 
         } catch(Exception e) {
             e.printStackTrace();
 
-        } finally {
-            if(connection != null) connection.disconnect();
         }
 
-        jsonMap = resultJson.findValue("items");
-        printJson(jsonMap.toString());
+        schema = resultJson.findValue("schema");
+        rows = resultJson.findValue("rows");
+
+        System.out.println(schema.toString());
+        System.out.println(rows.toString());
     }
 }
