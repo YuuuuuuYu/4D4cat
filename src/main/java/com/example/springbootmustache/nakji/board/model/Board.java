7901311fcf855001044f49050b74a4e4a39ede8a
Nakji <yuitfhk@gmail.com>
2023-11-27 10:49:50 +0900

    db 관련 세팅 보류

--- src/main/java/com/example/springbootmustache/nakji/board/model/Board.java
+++ src/main/java/com/example/springbootmustache/nakji/board/model/Board.java
@@ -2,35 +2,21 @@ package com.example.springbootmustache.nakji.board.model;
 
 import lombok.Getter;
 
-import javax.persistence.*;
 import java.util.Date;
 
 @Getter
-@Entity
-@Table(name = "Board")
 public class Board {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long board_id;
 
-    @Column
     private String title;
-    @Column
     private String content;
-    @Column
     private int writer_id;
-    @Column
     private Long view_cnt;
-    @Column
     private String use_yn;
 
-    @Column
     private int create_id;
-    @Column
     private Date create_date;
-    @Column
     private int update_id;
-    @Column
     private Date update_date;
 }
