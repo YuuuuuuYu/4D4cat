b9ca00173a4552ac649feaf05a00a929a0bc608b
Nakji <yuitfhk@gmail.com>
2023-11-29 17:10:21 +0900

    구글 빅쿼리를 이용한 날짜별 인기검색어 순위 테스트 추가

--- src/test/java/com/example/springbootmustache/nakji/Iface/GoogleBigqueryClient.java
+++ src/test/java/com/example/springbootmustache/nakji/Iface/GoogleBigqueryClient.java
@@ -5,14 +5,14 @@ import feign.Param;
 import feign.RequestLine;
 import feign.Response;
 
-public interface GoogleTopTermsClient {
+public interface GoogleBigqueryClient {
 
     @RequestLine("GET /bigquery/v2/projects/{projectId}/queries/{jobId}")
     @Headers({
             "Content-Type: application/json",
             "Authorization: Bearer {token}"
     })
-    Response getQuery(@Param("projectId") String projectId
-                    , @Param("jobId") String jobId
-                    , @Param("token") String token);
+    Response getQuery(@Param("token") String token
+                      , @Param("projectId") String projectId
+                      , @Param("jobId") String jobId);
 }
