d107e2bc45840c6ccb64950ef65e372ffe434291
Nakji <yuitfhk@gmail.com>
2024-03-28 17:11:54 +0900

    board 페이지 수정

--- src/main/java/com/example/springbootmustache/nakji/board/controller/BoardController.java
+++ src/main/java/com/example/springbootmustache/nakji/board/controller/BoardController.java
@@ -1,14 +1,23 @@
 package com.example.springbootmustache.nakji.board.controller;
 
 import com.example.springbootmustache.nakji.model.PageView;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/nakji/board")
 public class BoardController {
 
     @GetMapping("/")
-    public PageView index(PageView pageView) {
+    public String index(Model model) {
+
+        return "/nakji/lnb/board";
+    }
+
+    @GetMapping("/view")
+    public PageView getView(PageView pageView) {
 
         return pageView;
     }
