14af430aa363a1dde34808374434e21f7a5194a4
Nakji <yuitfhk@gmail.com>
2024-03-29 17:38:36 +0900

    실험실 메뉴에 마크다운 에디터 추가

--- src/main/java/com/example/springbootmustache/nakji/labs/controller/LabsController.java
+++ src/main/java/com/example/springbootmustache/nakji/labs/controller/LabsController.java
@@ -11,7 +11,13 @@ public class LabsController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("labsIntro", "/nakji/lnb/labs/labs_intro");
+        return "/nakji/lnb/labs";
+    }
 
+    @GetMapping("/md_editor")
+    public String md_editor(Model model) {
+        model.addAttribute("labsMdEditor", "/nakji/lnb/labs/labs_MD_Editor");
         return "/nakji/lnb/labs";
     }
 }
