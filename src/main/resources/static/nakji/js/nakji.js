d55f03199878cbb3f3f43eed250e7ba853d19032
Nakji <yeonghak77@kangwon.ac.kr>
2024-04-02 17:28:42 +0900

    불필요한 클래스 제거 및 페이징 호출 로직 수정

--- src/main/resources/static/nakji/js/nakji.js
+++ src/main/resources/static/nakji/js/nakji.js
@@ -4,11 +4,27 @@ window.onload = function () {
     renderingPage('/nakji/page/intro', '');
 };
 
-async function renderingPage(url, data) {
+async function renderingPage(url, script) {
     const template = await callAPIGet(url, '');
-    const page = Mustache.render(template, data);
+    const page = Mustache.render(template, '');
 
     document.getElementById('page').innerHTML = page;
+
+    if (script !== '') {
+        try {
+            const jsonForm = "\'"+script.replaceAll("\'", '\"') + "\'";
+            const scriptJson = JSON.parse(jsonForm);
+            const js = document.createElement('script');
+
+            js.type = scriptJson.type;
+            js.src = scriptJson.src;
+            document.body.appendChild(js);
+
+        } catch (error) {
+            console.error(error.message);
+            return {};
+        }
+    }
 }
 
 async function callAPIGet(url, params) {
