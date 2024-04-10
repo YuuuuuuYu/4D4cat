4a2d22edf5c5107abab630e8a279c620e2129995
Nakji <yeonghak77@kangwon.ac.kr>
2024-04-10 18:04:12 +0900

    openai gpt 소스 추가

---
+++ src/main/java/com/example/springbootmustache/nakji/api/client/OpenAIGptClient.java
@@ -0,0 +1,17 @@
+package com.example.springbootmustache.nakji.api.client;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import feign.Response;
+
+public interface OpenAIGptClient {
+
+    @RequestLine("POST /v1/chat/completions")
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8",
+            "Authorization: Bearer {AI_KEY}"
+    })
+    Response prompt(@Param("AI_KEY") String key, String body);
+
+}
