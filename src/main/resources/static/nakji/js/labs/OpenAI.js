e533285e7e373b351c007f59cc0c76694ea99168
Nakji <yeonghak77@kangwon.ac.kr>
2024-04-29 16:55:19 +0900

    프론트단 중복 호출 방지 및 로딩중 표시 추가

--- src/main/resources/static/nakji/js/labs/OpenAI.js
+++ src/main/resources/static/nakji/js/labs/OpenAI.js
@@ -3,12 +3,24 @@ window.openAI = {
         openAI.setConvertBtn();
     },
     call: async function() {
+        const callButton = this;
+        const loading = document.getElementById('btn-loading');
+        callButton.disabled = true; // 버튼 비활성화
+        callButton.classList.add('button-disabled'); // 로딩 중 스타일 적용
+        callButton.innerText = 'Loading...'; // 버튼 텍스트 변경
+        loading.classList.remove('hidden'); // 로딩 애니메이션 표시
+
         const prompt = document.getElementById('prompt-text').value;
         const url = '/nakji/api/gpt?prompt='+prompt;
         const result = await callAPIGet(url, '');
 
         document.querySelector('.response-area-content').innerHTML = result.data;
         document.querySelector('.response-time').innerHTML = result.time;
+
+        callButton.disabled = false;
+        callButton.classList.remove('button-disabled'); // 원래 스타일로 복원
+        callButton.innerText = 'Call'; // 버튼 텍스트 복원
+        loading.classList.add('hidden'); // 로딩 애니메이션 숨김
     },
     setConvertBtn: function() {
         document.getElementById('btn-call').addEventListener('click', openAI.call);
