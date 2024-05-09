9edbda79067e5d206508436d6604fb4496e5c951
Nakji <yuitfhk@gmail.com>
2024-05-09 17:35:11 +0900

    apiService 단위 테스트

---
+++ src/test/java/com/example/springbootmustache/nakji/api/service/ApiServiceTest.java
@@ -0,0 +1,75 @@
+package com.example.springbootmustache.nakji.api.service;
+
+import com.example.springbootmustache.nakji.api.client.OpenAIGptClient;
+import com.example.springbootmustache.nakji.common.CommonService;
+import com.example.springbootmustache.nakji.config.NakjiProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import feign.Feign;
+import feign.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ApiServiceTest {
+
+    @Mock
+    private NakjiProperty nakji;
+
+    @Mock
+    private CommonService commonService;
+
+    @InjectMocks
+    private ApiService apiService;
+
+    @BeforeEach
+    void setup() {}
+
+    @Test
+    void naverSearch() {
+    }
+
+    @Test
+    void googleSearch() {
+    }
+
+    @Test
+    @DisplayName("OPENAI Gpt-4 Callboack Test")
+    void openAIGpt() throws IOException {
+        String prompt = "안녕하세요";
+        String url = "https://api.openai.com";
+        String OPENAI_KEY = ""; // Test 할 때만 기입
+        String model = "gpt-4";
+        String requestBody = "{\"messages\":[{\"role\":\"user\", \"content\":\""+prompt+"\"}],\"model\":\""+model+"\"}";
+
+        // given
+        OpenAIGptClient client = Feign.builder().target(OpenAIGptClient.class, url);
+        Response mockResponse = client.prompt(OPENAI_KEY, prompt);
+
+        // when
+        when(nakji.openAIGptConnection(requestBody)).thenReturn(mockResponse);
+        String resultMsg = apiService.openAIGpt(prompt);
+
+        assertNull(apiService.openAIGpt(prompt));
+        assertEquals(resultMsg, responseToJson(mockResponse));
+        verify(apiService).openAIGpt(prompt);
+    }
+
+    String responseToJson(Response response) throws IOException {
+        JsonNode toJson = commonService.readBody(response.body().asInputStream());
+        JsonNode resultJson = toJson.at("/choices/0/message/content");
+
+        return resultJson.toString();
+    }
+}
