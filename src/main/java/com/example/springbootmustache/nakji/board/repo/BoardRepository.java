72aa74bb92e5d54648cff1057583c815fa881354
Nakji <yeonghak77@kangwon.ac.kr>
2024-05-14 14:38:46 +0900

    Board 엔티티 수정

--- src/main/java/com/example/springbootmustache/nakji/board/repo/BoardRepository.java
+++ src/main/java/com/example/springbootmustache/nakji/board/repo/BoardRepository.java
@@ -3,9 +3,4 @@ package com.example.springbootmustache.nakji.board.repo;
 import com.example.springbootmustache.nakji.board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-    Optional<Board> findByBoardId(Long id);
-}
+public interface BoardRepository extends JpaRepository<Board, Long> { }
