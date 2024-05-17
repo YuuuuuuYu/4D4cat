9b95a59a8e8b1b35aecd86fc259cf555ddbe7491
Nakji <yeonghak77@kangwon.ac.kr>
2024-05-17 17:57:22 +0900

    Board 데이터 추가/수정/조회 및 공통 서비스단 상속

--- src/main/java/com/example/springbootmustache/nakji/board/service/BoardService.java
+++ src/main/java/com/example/springbootmustache/nakji/board/service/BoardService.java
@@ -4,20 +4,57 @@ import com.example.springbootmustache.nakji.board.model.Board;
 import com.example.springbootmustache.nakji.board.model.BoardDto;
 import com.example.springbootmustache.nakji.board.repo.BoardRepository;
 import com.example.springbootmustache.nakji.common.CommonService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class BoardService {
-
-    @Autowired
-    CommonService commonService;
+public class BoardService extends CommonService{
 
     @Autowired
     private BoardRepository boardRepository;
 
     public BoardDto getBoardById(Long boardId) {
         Board board = boardRepository.findById(boardId).orElseThrow(() -> new RuntimeException("Board not found"));
+        return BoardDto.entityToDto(board);
+    }
+
+    public List<BoardDto> findAll() {
+        List<Board> boards = boardRepository.findAll();
+        return boards.stream().map(BoardDto::entityToDto).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public BoardDto createBoard(BoardDto boardDto) {
+        Board board = new Board();
+        board.setTitle(boardDto.getTitle());
+        board.setContent(boardDto.getContent());
+        board.setViewCnt(0L);
+        board.setUseYn("Y");
+        board.setCreateId(boardDto.getWriterId());
+        board.setCreateDate(new Date());
+        board.setUpdateId(boardDto.getWriterId());
+        board.setUpdateDate(new Date());
+        boardRepository.save(board);
+
+        return BoardDto.entityToDto(board);
+    }
+
+    @Transactional
+    public BoardDto updateBoard(Long boardId, BoardDto boardDto) {
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new EntityNotFoundException("Board not found"));
+        board.setTitle(boardDto.getTitle());
+        board.setContent(boardDto.getContent());
+        board.setUseYn(boardDto.getUseYn());
+        board.setUpdateId(boardDto.getWriterId());
+        board.setUpdateDate(new Date());
+        boardRepository.save(board);
+
         return BoardDto.entityToDto(board);
     }
 }
