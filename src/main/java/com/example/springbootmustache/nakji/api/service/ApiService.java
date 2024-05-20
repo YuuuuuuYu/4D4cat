68d1154381dd8e8a145f3906283bff66a3e3e6a1
Nakji <yuitfhk@gmail.com>
2024-05-20 16:30:49 +0900

    프로퍼티 내 api 호출 서비스 분리

--- src/main/java/com/example/springbootmustache/nakji/api/service/ApiService.java
+++ src/main/java/com/example/springbootmustache/nakji/api/service/ApiService.java
@@ -1,12 +1,12 @@
 package com.example.springbootmustache.nakji.api.service;
 
 import com.example.springbootmustache.nakji.common.CommonService;
-import com.example.springbootmustache.nakji.config.NakjiProperty;
 import com.example.springbootmustache.nakji.model.SearchForm;
 import com.fasterxml.jackson.databind.JsonNode;
 import feign.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
@@ -18,20 +18,16 @@ import java.util.Optional;
 @Service
 public class ApiService {
     private static final Logger log = LoggerFactory.getLogger(ApiService.class);
-    private final NakjiProperty nakji;
-    private final CommonService commonService;
 
-    public ApiService(CommonService commonService, NakjiProperty nakji) {
-        this.commonService = commonService;
-        this.nakji = nakji;
-    }
+    @Autowired
+    private CommonService commonService;
 
     public List<SearchForm> naverSearch(String serviceId, String query) {
         JsonNode resultJson = null;
         JsonNode jsonMap = null;
         List<SearchForm> returnPage = null;
 
-        try (Response response = nakji.naverSearchConnection(serviceId, query)) {
+        try (Response response = commonService.naverSearchConnection(serviceId, query)) {
             if (response.status() == 200){
                 returnPage = new ArrayList<>();
                 resultJson = commonService.readBody(response.body().asInputStream());
@@ -57,7 +53,7 @@ public class ApiService {
         JsonNode jsonMap = null;
         List<SearchForm> returnPage = null;
 
-        try (Response response = nakji.googleSearchConnection(query)) {
+        try (Response response = commonService.googleSearchConnection(query)) {
             if (response.status() == 200){
                 returnPage = new ArrayList<>();
                 resultJson = commonService.readBody(response.body().asInputStream());
@@ -82,7 +78,7 @@ public class ApiService {
         String scope = "https://www.googleapis.com/auth/bigquery";
         String redirect_uri = "http://localhost:8888/nakji/api"+param;
 
-        return nakji.activeCodeApi(scope, redirect_uri);
+        return commonService.activeCodeApi(scope, redirect_uri);
     }
 
     public String getAccessToken(String code) {
@@ -90,7 +86,7 @@ public class ApiService {
         String accessToken = null;
         JsonNode resultJson = null;
 
-        try (Response response = nakji.accessTokenApi(redirect_uri, code)) {
+        try (Response response = commonService.accessTokenApi(redirect_uri, code)) {
             if (response.status() == 200) {
                 resultJson = commonService.readBody(response.body().asInputStream());
                 accessToken = resultJson.findValue("access_token").textValue();
@@ -114,7 +110,7 @@ public class ApiService {
         String model = "gpt-4";
         String requestBody = "{\"messages\":[{\"role\":\"user\", \"content\":\""+prompt+"\"}],\"model\":\""+model+"\"}";
 
-        try (Response response = nakji.openAIGptConnection(requestBody)) {
+        try (Response response = commonService.openAIGptConnection(requestBody)) {
             if (response.status() == 200){
                 resultJson = commonService.readBody(response.body().asInputStream());
                 jsonMap = resultJson.at("/choices/0/message/content");
--- src/main/java/com/example/springbootmustache/nakji/config/NakjiProperty.java
+++ src/main/java/com/example/springbootmustache/nakji/config/NakjiProperty.java
@@ -1,14 +1,6 @@
 package com.example.springbootmustache.nakji.config;
 
 import com.example.springbootmustache.common.UserProperties;
-import com.example.springbootmustache.nakji.api.client.GoogleAccessTokenClient;
-import com.example.springbootmustache.nakji.api.client.GoogleSearchClient;
-import com.example.springbootmustache.nakji.api.client.NaverSearchClient;
-import com.example.springbootmustache.nakji.api.client.OpenAIGptClient;
-import feign.Feign;
-import feign.Response;
-import feign.gson.GsonDecoder;
-import feign.gson.GsonEncoder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -61,6 +53,34 @@ public class NakjiProperty extends UserProperties {
         OPENAI_KEY = openai_key;
     }
 
+    public static String getNaverId() {
+        return NAVER_ID;
+    }
+
+    public static String getNaverKey() {
+        return NAVER_KEY;
+    }
+
+    public static String getGoogleKey() {
+        return GOOGLE_KEY;
+    }
+
+    public static String getGoogleCx() {
+        return GOOGLE_CX;
+    }
+
+    public static String getGoogleClientId() {
+        return GOOGLE_CLIENT_ID;
+    }
+
+    public static String getGoogleClientSecret() {
+        return GOOGLE_CLIENT_SECRET;
+    }
+
+    public static String getOpenaiKey() {
+        return OPENAI_KEY;
+    }
+
     public String getImgUrl() {
         return imgUrl;
     }
@@ -68,123 +88,4 @@ public class NakjiProperty extends UserProperties {
         return srcUrl;
     }
 
-    /*public HttpURLConnection naverSearchConnection(String id, String search) {
-        String query = "".equals(nvl(search)) ? "bag" : search;
-        String serviceId = "".equals(id) ? "blog.json" : id;
-        String host = "https://openapi.naver.com/v1/search/"+serviceId;
-
-        URL url = null;
-        HttpURLConnection connection = null;
-        try {
-            url = new URL(host+"?query="+ URLEncoder.encode(query, "UTF-8"));
-            connection = (HttpURLConnection) url.openConnection();
-
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            connection.setRequestProperty("X-Naver-Client-Id", NAVER_ID);
-            connection.setRequestProperty("X-Naver-Client-Secret", NAVER_KEY);
-
-        } catch(Exception e) {
-            e.printStackTrace();
-
-        }
-
-        return connection;
-    }
-
-    public HttpURLConnection googleSearchConnection(String search) {
-        String query = "".equals(nvl(search)) ? "bag" : search;
-        String host = "https://www.googleapis.com/customsearch/v1";
-
-        URL url = null;
-        HttpURLConnection connection = null;
-        try {
-            url = new URL(host+"?key="+GOOGLE_KEY+"&cx="+GOOGLE_CX+"&q="+ URLEncoder.encode(query, "UTF-8"));
-            connection = (HttpURLConnection) url.openConnection();
-
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-
-        }
-
-        return connection;
-    }*/
-
-    public Response naverSearchConnection(String id, String search) {
-        Response response = null;
-        String query = "".equals(nvl(search)) ? "bag" : search;
-        String serviceId = "".equals(nvl(id)) ? "blog.json" : id;
-        String url = "https://openapi.naver.com";
-
-        NaverSearchClient client = Feign.builder()
-                                    .encoder(new GsonEncoder())
-                                    .decoder(new GsonDecoder())
-                                    .target(NaverSearchClient.class, url);
-
-        response = client.search(NAVER_ID, NAVER_KEY, serviceId, query);
-
-        return response;
-    }
-
-    public Response googleSearchConnection(String search) {
-        Response response = null;
-        String query = "".equals(nvl(search)) ? "bag" : search;
-        String url = "https://www.googleapis.com";
-
-        GoogleSearchClient client = Feign.builder()
-                                    .encoder(new GsonEncoder())
-                                    .decoder(new GsonDecoder())
-                                    .target(GoogleSearchClient.class, url);
-
-        response = client.search(GOOGLE_KEY, GOOGLE_CX, query);
-
-        return response;
-    }
-
-    public String activeCodeApi(String scope, String redirect_uri) {
-        String url = "https://accounts.google.com/o/oauth2/v2/auth";
-        String response_type = "code";
-
-        StringBuilder uri = new StringBuilder(url);
-        uri.append("?scope=").append(scope)
-                .append("&response_type=").append(response_type)
-                .append("&client_id=").append(GOOGLE_CLIENT_ID)
-                .append("&redirect_uri=").append(redirect_uri);
-
-        return uri.toString();
-    }
-
-    public Response accessTokenApi(String redirect_uri, String activeCode) {
-        Response response = null;
-        String url = "https://oauth2.googleapis.com";
-        String grand_type = "authorization_code";
-
-        StringBuilder param = new StringBuilder();
-
-        GoogleAccessTokenClient client = Feign.builder()
-                                        .target(GoogleAccessTokenClient.class, url);
-
-        param.append("code=").append(activeCode)
-            .append("&client_id=").append(GOOGLE_CLIENT_ID)
-            .append("&client_secret=").append(GOOGLE_CLIENT_SECRET)
-            .append("&redirect_uri=").append(redirect_uri)
-            .append("&grant_type=").append(grand_type);
-
-        response = client.getAccessToken(param.toString());
-
-        return response;
-    }
-
-    public Response openAIGptConnection(String prompt) {
-        Response response = null;
-        String url = "https://api.openai.com";
-
-        OpenAIGptClient client = Feign.builder().target(OpenAIGptClient.class, url);
-        response = client.prompt(OPENAI_KEY, prompt);
-
-        return response;
-    }
 }
